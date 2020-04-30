3fb4e8ebcba71aa95bc9e225829c1fc28114b6fa
Allan Kadous <kadous.allan@outlook.fr>
2020-04-30 11:57:34 +0200

    allan

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
 
 
